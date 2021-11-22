e0f95fd9c023a680f74b71d4e0a4830a44b0422c
Wire <whyyoulittle333@gmail.com>
2021-11-21 23:13:32 -0600

    Add naming

--- fridgeUpload.sh
+++ fridgeUpload.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-while true  
+set -e
+
+if [ ! -f ./fridgeName ]; then
+   echo "fridgeName file does not exist"
+   exit 0
+fi
+gridderName=$(cat fridgeName)
+
+while true
 do
   if [ -f /home/pi/fridge-hfdc/fridgeOpens.csv ]; then
-  curl -T /home/pi/fridge-hfdc/fridgeOpens.csv http://wire.my.to:81/backup/
+  curl -T /home/pi/fridge-hfdc/fridgeOpens.csv http://wire.my.to:81/fridge-hfdc/${gridderName}.csv
   echo "Uploaded"
   fi
   sleep 300
