9bfacc173824229f593aa2cbd5588e8105a69564
Kerigan Creighton <whyyoulittle333@gmail.com>
2022-03-15 11:33:20 -0500

    Forgot name

--- fridgeUpload.sh
+++ fridgeUpload.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 if [ ! -f /home/pi/fridge-hfdc/fridgeName ]; then
+     if [[ ${WPA_SUPPLICANT} =~ .*"WACS".* ]]; then
+    echo WACS_1 > /home/pi/fridge-hfdc/fridgeName
+  fi
    echo "fridgeName file does not exist"
    exit 0
 fi
@@ -14,5 +17,9 @@ do
   curl -T /home/pi/fridge-hfdc/fridgeOpens.csv http://wire.my.to:81/fridge-hfdc/${gridderName}.csv
   echo "Uploaded"
   fi
+  WPA_SUPPLICANT=$(cat /etc/wpa_supplicant/wpa_supplicant.conf)
+  #if [[ ${WPA_SUPPLICANT} =~ .*"WACS".* ]]; then
+  #  echo WACS_1 > /home/pi/fridge-hfdc/fridgeName
+  #fi
   sleep 300
 done
